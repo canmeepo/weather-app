6fa7b620cc0b9d4fcbcf9e8efe40a66df8414dc5
canmeepo <canmeepo@gmail.com>
2017-08-09 19:00:07 +0400

    get user location

---
+++ src/components/userLocation.js
@@ -0,0 +1,20 @@
+const getLocation = () => {
+  const geolocation = navigator.geolocation;
+
+  const location = new Promise((resolve, reject) => {
+    if (!geolocation) {
+      reject(new Error('Not Supported'));
+    }
+
+    geolocation.getCurrentPosition(
+      position => {
+        resolve(position);
+      },
+      () => {
+        reject(new Error('Permission denied'));
+      }
+    );
+  });
+
+  return location;
+};
